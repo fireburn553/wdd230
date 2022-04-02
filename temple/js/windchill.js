8fd723730f02ec5c86188265a95f7b55dcda6984
James Charlie Salva <JamesSalvajames@hotmail.com>
2022-04-02 23:21:11 +0800

    added current weather

---
+++ temple/js/windchill.js
@@ -0,0 +1,61 @@
+
+
+const weatherAPIurl = `https://api.openweathermap.org/data/2.5/weather?q=Daet&units=imperial&appid=b8db4725cc0def9c2c61b3b8800603a3`;
+
+const currentWeatherContainer = document.querySelector(".current-weather");
+
+fetch(weatherAPIurl)
+    .then((response) => response.json())
+    .then((jsObject) => {
+        
+        //Current Temperature
+        let currentTemp = jsObject.main.temp.toFixed(1);
+        let temp = document.createElement('h4');
+        temp.textContent = `${currentTemp} \xB0F`;
+        currentWeatherContainer.appendChild(temp);
+
+        //Weather Icon
+        const iconsrc = `https://openweathermap.org/img/wn/${jsObject.weather[0].icon}@2x.png`;
+        document.createElement('img').setAttribute('src', iconsrc);
+        
+        //Weather Description
+        let desc = jsObject.weather[0].description;
+        desc = desc.split(' ').map(capitalize).join(' ');
+        
+        //Figure Caption
+        document.querySelector('figcaption').textContent = desc;
+        document.querySelector('#weather-icon').setAttribute('alt', desc);
+
+        //Wind Speed
+        let windSpeed = jsObject.wind.speed;
+        document.querySelector('#speed').textContent = windSpeed;
+
+         // Credits
+        const weatherWebsite = "https://openweathermap.org/"
+        document.querySelector('#credit').textContent = weatherWebsite;
+
+        computerWindChill(currentTemp, windSpeed);
+
+    
+    });
+
+function capitalize(word) {
+    return `${word.charAt(0).toUpperCase()}${word.slice(1)}`;
+}
+
+function computerWindChill(tempNumber, speedNumber){
+    let windChill = Math.round(35.74 + (0.6215*tempNumber) - (35.75*Math.pow(speedNumber, 0.16)) + (0.4275*tempNumber*Math.pow(speedNumber, 0.16)));
+
+if (tempNumber<=50 && speedNumber>3) {
+    document.getElementById("chill").textContent = "Wind Chill is "+windChill+"\xB0F"+mph;
+} else {
+    document.getElementById("chill").textContent = "N/A";
+}
+}
+
+
+
+//more than three
+////speed>3
+//50 degree fareinheit or colder
+////temp <= 50
