35d86bc9e839a47edbd81c473f5d00614e4fcb7f
James Charlie Salva <JamesSalvajames@hotmail.com>
2022-04-05 00:54:40 +0800

    changes

--- temple/js/script.js
+++ temple/js/script.js
@@ -17,6 +17,9 @@ const Name = document.querySelector(".temple-card-header");
 const templeImg = document.querySelector(".temple-card-image");
 const templeDetails = document.querySelector(".temple-card-body");
 
+const Name2 = document.querySelector(".temple-card2-header");
+const templeImg2 = document.querySelector(".temple-card2-image");
+const templeDetails2 = document.querySelector(".temple-card2-body");
 fetch(requestURL)
     .then(function(response) {
         return response.json();
@@ -25,7 +28,15 @@ fetch(requestURL)
         const directory = jsonObject['directory'];
         
         let randomNum = Math.floor(Math.random()*directory.length);
+        let anotherNum = randomNum+1;
         insertTemple(directory[randomNum]);
+        if (anotherNum>3){
+            insertTemple2(directory[0]);
+        }else{
+            insertTemple2(directory[anotherNum]);
+            
+        }
+        
     })
 
 function insertTemple(temple){
@@ -48,5 +59,26 @@ function insertTemple(temple){
     templeDetails.appendChild(address);
     templeDetails.appendChild(telephone);
     templeDetails.appendChild(email);
-    
+}
+
+function insertTemple2(temple){
+    let templeName = document.createElement('h3');
+    let templeImage = document.createElement('img');
+    let address = document.createElement('p');
+    let telephone = document.createElement('p');
+    let email = document.createElement('p');
+
+    //Change the Content property of the h2 Element
+    templeName.textContent = `${temple.name}`;
+    templeImage.setAttribute('src', temple.image);
+    templeImage.setAttribute('alt', `${temple.name} Image`);
+    address.textContent = `Address: ${temple.addresses}`;
+    telephone.textContent = `Telephone: ${temple.phone}`;
+    email.textContent = `Email: ${temple.email}`;
+    //Add/append the div to the contents
+    Name2.appendChild(templeName);
+    templeImg2.appendChild(templeImage);
+    templeDetails2.appendChild(address);
+    templeDetails2.appendChild(telephone);
+    templeDetails2.appendChild(email);
 }
